b50f022e90a90e29dcf5982f7b94926791fecbdb
hyun-soo choi <hyun-soochoi@hyun-sooui-MacBookAir.local>
2021-08-12 20:08:39 +0900

    Add: Django Project Setting

---
+++ my_settings.py
@@ -0,0 +1,12 @@
+DATABASES = {
+    'default' : {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'starbucks',
+        'USER': 'root',
+        'PASSWORD': 'forTIS93!',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+    }
+}
+
+SECRET_KEY = '시크릿키'
